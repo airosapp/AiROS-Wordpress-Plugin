5f78c48ea3b79f3ca28aa8113d791e7bfa5df333
airosapp <156965895+airosapp@users.noreply.github.com>
2024-06-25 23:41:34 -0700

    Create admin-script.js

---
+++ assets/js/admin-script.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function($) {
+    $('.nav-tab').click(function(event) {
+        event.preventDefault();
+        $('.nav-tab').removeClass('nav-tab-active');
+        $(this).addClass
