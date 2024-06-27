61a1163733730e817428d0577e305d82b467de7c
airosapp <156965895+airosapp@users.noreply.github.com>
2024-06-26 18:31:33 -0700

    Update live-chat-script.js

--- assets/js/live-chat-script.js
+++ assets/js/live-chat-script.js
@@ -1,5 +1,12 @@
 jQuery(document).ready(function($) {
-    $('#airos-live-chat-button').click(function() {
+    $('#airos-live-chat-button').on('click', function() {
         $('#airos-live-chat-modal').toggle();
+    });
+
+    // Hide the chat modal when clicking outside of it
+    $(document).on('click', function(e) {
+        if (!$(e.target).closest('#airos-live-chat-modal, #airos-live-chat-button').length) {
+            $('#airos-live-chat-modal').hide();
+        }
     });
 });
