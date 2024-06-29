15cd4257d677a422bdd41dc0d13de5d04d4288c7
airosapp <156965895+airosapp@users.noreply.github.com>
2024-06-29 14:57:55 -0700

    Update admin-page-template.php

--- admin-page-template.php
+++ admin-page-template.php
@@ -1,10 +1,11 @@
 <!-- admin-page-template.php -->
 <div class="wrap airos-wrap">
     <div class="airos-admin-header">
-        <img src="https://airosapp.com/wp-content/uploads/2024/03/AiROS-Logo-copy.png" alt="AiROS Logo" class="airos-logo">
-        <h2>AiROS App</h2>
+        <h2></h2>
     </div>
     <div class="airos-admin-content">
+        <img src="https://airosapp.com/wp-content/uploads/2024/03/AiROS-Logo-copy.png" alt="AiROS Logo" class="airos-logo">
+		
         <form action='options.php' method='post'>
             <?php
             settings_fields('airosApp');
@@ -14,3 +15,4 @@
         </form>
     </div>
 </div>
+
