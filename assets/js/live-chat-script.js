c3a8ea38d03e6c572105e4f07033705a2a79b903
airosapp <156965895+airosapp@users.noreply.github.com>
2024-07-28 13:01:53 -0700

    Update live-chat-script.js

--- assets/js/live-chat-script.js
+++ assets/js/live-chat-script.js
@@ -1,39 +1,31 @@
 // JavaScript to handle the live chat button click
-add_action('wp_footer', function() {
-    ?>
-    <script>
-    // JavaScript to handle the live chat button click
-    jQuery(document).ready(function($) {
-        $('#airos-live-chat-button-icon, #airos-live-chat-button-text').click(function() {
-            var $modal = $('#airos-live-chat-modal');
-            var $button = $(this);
-            var $icon = $button.find('.chat-icon');
-            var $text = $button.find('.chat-text');
-            var $close = $button.find('.chat-close');
+jQuery(document).ready(function($) {
+    $('#airos-live-chat-button-icon, #airos-live-chat-button-text').click(function() {
+        var $modal = $('#airos-live-chat-modal');
+        var $button = $(this);
+        var $icon = $button.find('.chat-icon');
+        var $text = $button.find('.chat-text');
+        var $close = $button.find('.chat-close');
 
-            $modal.toggleClass('open');
-            $button.toggleClass('active');
+        $modal.toggleClass('open');
+        $button.toggleClass('active');
 
-            if ($modal.hasClass('open')) {
-                $icon.hide();
-                $text.hide();
-                $close.css('display', 'flex'); // Show the close button as a flexbox item
-                $modal.css({
-                    'opacity': '1',
-                    'visibility': 'visible'
-                });
-            } else {
-                $icon.show();
-                $text.show();
-                $close.hide();
-                $modal.css({
-                    'opacity': '0',
-                    'visibility': 'hidden'
-                });
-            }
-        });
+        if ($modal.hasClass('open')) {
+            $icon.hide();
+            $text.hide();
+            $close.css('display', 'flex'); // Show the close button as a flexbox item
+            $modal.css({
+                'opacity': '1',
+                'visibility': 'visible'
+            });
+        } else {
+            $icon.show();
+            $text.show();
+            $close.hide();
+            $modal.css({
+                'opacity': '0',
+                'visibility': 'hidden'
+            });
+        }
     });
-    </script>
-    <?php
 });
-?>
