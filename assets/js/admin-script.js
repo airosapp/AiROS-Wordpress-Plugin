c47efbe6d19ee393f80ffdcaea4f4541fdffc741
airosapp <156965895+airosapp@users.noreply.github.com>
2024-07-28 19:48:12 -0700

    Update admin-script.js

--- assets/js/admin-script.js
+++ assets/js/admin-script.js
@@ -12,4 +12,25 @@ jQuery(document).ready(function($) {
 
     // Show the first tab by default
     $('#general').show();
+
+    // New functionality for 'All Day' checkbox
+    function toggleTimeInputs(checkbox) {
+        var dayTimeInputsWrapper = checkbox.closest('.airos-day-schedule').querySelector('.day-time-inputs-wrapper');
+        if (checkbox.checked) {
+            dayTimeInputsWrapper.style.display = 'none';
+        } else {
+            dayTimeInputsWrapper.style.display = 'flex';
+        }
+    }
+
+    var checkboxes = document.querySelectorAll('.all-day-checkbox');
+    checkboxes.forEach(function(checkbox) {
+        // Initial toggle based on current state
+        toggleTimeInputs(checkbox);
+
+        // Attach change event listener
+        checkbox.addEventListener('change', function() {
+            toggleTimeInputs(checkbox);
+        });
+    });
 });
